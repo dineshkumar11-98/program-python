c4e90b31908c7ea65771583a415f72e2639fa863
dinesh kumar <54545814+dineshkumar11-98@users.noreply.github.com>
2019-08-27 21:17:17 +0530

    Add files via upload

---
+++ dictonary.py
@@ -0,0 +1,4 @@
+lis={'thala':'ajith kumar','thalapathy':'vijay','super_star':'rajinikanth','ulakanayakan':'kamal kassan'}
+print("Enter the nick name of actors")
+x=input()
+print("Real name is:",lis[x])
