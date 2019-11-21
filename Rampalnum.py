920a2f6eb00510af7130d5de0115e8d08abb19c1
dinesh kumar <54545814+dineshkumar11-98@users.noreply.github.com>
2019-11-21 20:13:52 +0530

    Update Rampalnum.py

--- Rampalnum.py
+++ Rampalnum.py
@@ -1,1 +1,18 @@
 
+a=input()
+s=len(a)
+
+
+d=int(a)
+if(d<0):
+	c=int(a[1])+int(a[s-1])
+	if(c%4==0):
+		print('Yes')
+	else:
+		print('No')
+elif(d>0):
+    e=int(a[s-2])+int(a[s-1])
+    if(e%4==0 and e/4<1):
+        print('Yes')
+    else:
+        print('No')
