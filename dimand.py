263d722203a2c9129659020db00c72e409cb8ef6
dinesh kumar <54545814+dineshkumar11-98@users.noreply.github.com>
2019-11-21 19:51:20 +0530

    Update dimand.py

--- dimand.py
+++ dimand.py
@@ -2,18 +2,18 @@ a=1
 l=[]
 v=' '
 c=5
-print("      1")
+print("        1")
 for x in range(6):
         a=a+1
         l.append(a)
         d=v*c
-        print(d,"1",*l,"1",sep='')
+        print(d,"1",*l,"1",sep=' ')
         c=c-1
 b=len(l)
 f=0
 for y in range(6):
         e=v*f
-        print(e,"1",*l[:b:1],"1",sep='')
+        print(e,"1",*l[:b:1],"1",sep=' ')
         b=b-1
         f=f+1
-print("      1")
+print("        1")
